4cd4affe348d68a17554a498a2522bc3239fa2f0
zarnam-izhar <164419157+zarnam-izhar@users.noreply.github.com>
2024-07-20 00:21:50 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,80 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+let todo = []
+let condition = true;
+while(condition)
+
+{let todoQuestions = await inquirer.prompt(
+    [
+
+      {
+        name:"select",
+        type: "list",
+        message: "Select an operation",
+        choice: ["Add", "Update", "View", "Delete", "Exit"]
+    
+
+    }]
+);
+if(todoQuestions.select === "Add"){
+    let addTodo = await inquirer.prompt([{
+        name:"todo",
+        type: "input",
+        message: "Add items in the list",
+        validate: function(input){
+            if(input.trim()== "") {
+                return "Please enter a nonempty item."}
+                return true;
+        }
+    }])
+    if (addTodo.todo.trim()!== ""){
+       
+    todo.push(addTodo.todo);
+    todo.forEach( todo => console.log(todo)
+    )
+    };
+}
+
+if(todoQuestions.select === "Update"){
+    let updateTodo = await inquirer.prompt([{
+        name: "todo",
+        type: "list",
+        message: "Update items in the list",
+        choices: todo.map(item => item)
+    }])
+    let addTodo = await inquirer.prompt([{
+        name:"todo",
+        type: "input",
+        message: "Add items in the list",
+    }])
+    let newTodo:any = todo.filter(val => val !== updateTodo.todo);
+    todo = [...newTodo,addTodo.todo];
+    todo.forEach( todo => console.log(todo)
+    )
+}
+
+if(todoQuestions.select === "View"){
+    console.log("*****TO Do List*****")
+    todo.forEach( todo => console.log(todo)
+    )
+}
+
+if(todoQuestions.select === "Delete"){
+    let deleteTodo = await inquirer.prompt([{
+        name: "todo",
+        type: "list",
+        message: "Select item to delete",
+        choices: todo.map(item => item)
+    }])
+    let newTodo:any = todo.filter(val => val !== deleteTodo.todo);
+    todo = [...newTodo];
+    todo.forEach( todo => console.log(todo)
+)
+}
+
+if(todoQuestions.select === "Exit"){
+console.log("Exiting program...");
+condition = false;
+}
+}
